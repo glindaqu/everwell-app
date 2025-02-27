0e97770b4f7f76687c531c7ab7012df1e2e7b531
glindaqu <q5481996@gmail.com>
2025-02-28 00:33:23 +0700

    uiState update bug fixed

--- app/src/main/java/ru/glindaquint/everwell/network/RetrofitFactory.kt
+++ app/src/main/java/ru/glindaquint/everwell/network/RetrofitFactory.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.glindaquint.everwell.network.interceptors.AuthorizationInterceptor
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @ActivityRetainedScoped
@@ -42,6 +43,8 @@ class RetrofitFactory
                 client =
                     OkHttpClient
                         .Builder()
+                        .readTimeout(10, TimeUnit.SECONDS)
+                        .connectTimeout(10, TimeUnit.SECONDS)
                         .addInterceptor(authorizationInterceptor)
                         .build()
             }
